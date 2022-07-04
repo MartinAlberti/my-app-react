1fc8364307fdb16712651f8081efdbd083b4617b
Martin Alberti <martinalberti123@gmail.com>
2022-07-04 18:02:37 -0300

    Catálogo con MAPS y Promises

--- src/componentes/ItemList.jsx
+++ src/componentes/ItemList.jsx
@@ -6,13 +6,14 @@ import Item from './Item'
 
 const ItemList = ({ products }) => {
     return (
-        <div className='flex justify-evenly mt-20 '>
-            < Item item={products.map((product) => <ul className='' key={product.id}>
+        <div className='flex justify-evenly mt-20   '>
+            < Item item={products.map((product) => <ul className=' shadow-sm p-4 ' key={product.id}>
                 <li className='flex justify-center text-3xl mb-8 '> {product.title}</li>
-                <li className='flex justify-center' > <img className='w-60 h-60 ' src={product.pictureUrl} alt="" /> </li>            
-                <li className='flex justify-center'> {product.description}</li>
-                <li className='flex justify-center '> ${product.price}</li>
+                <li className='flex justify-center mb-4' > <img className='w-60 h-60 ' src={product.pictureUrl} alt="" /> </li>
+                <li className='flex justify-center mb-8 w-80'> {product.description}</li>
+                <li className='flex justify-end text-xl '> ${product.price}</li>          
             </ul>)} />
+           
 
         </div>
 
