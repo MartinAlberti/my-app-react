99cfea70db79e2cdf94eba8dab0dbe6d99a6ad6e
Martin Alberti <martinalberti123@gmail.com>
2022-07-11 22:27:40 -0300

    primer preentrega

--- src/componentes/ItemList.jsx
+++ src/componentes/ItemList.jsx
@@ -11,9 +11,8 @@ const ItemList = ({ products }) => {
             < Item item={products.map((product) => <ul className=' shadow-sm p-4 w-80 h-120 ' key={product.id}>
                 <li className='flex justify-center text-2xl mb-8 '> {product.title}</li>
                 <li className='flex justify-center mb-4' > <img className='w-60 h-60 ' src={product.pictureUrl} alt="" /> </li>
-                <li className='flex justify-center mb-8 w-80'> {product.description}</li>
                 <li className='flex justify-end self-end text-xl '> ${product.price}</li>  
-                <li><Link to="/item:id">  ver detalles  </Link>   </li>   
+                <li> <Link to="/item">  ver detalles  </Link>   </li>   
             </ul>)} />        
 
         </div>
